e18a4a3313fc61eec1e02d4d9a9b6e32b6ed8d2e
jonandgon <jag73155@usc.edu>
2023-12-07 15:17:43 -0500

    Update Versus.tsx

--- src/components/Versus.tsx
+++ src/components/Versus.tsx
@@ -13,7 +13,7 @@ function Versus({ versus, isChallenger = true }: VersusProps) {
   return (
     <div className='flex flex-col mt-5 self-center whitespace-nowrap text-center text-md font-bold text-white items-center gap-1'>
       {isChallenger && 'You are challenging'}
-      <div className='w-[200px] max-w-full whitespace-nowrap rounded-[200px] border-4 border-solid border-bg2 bg-zinc-50 px-4 py-2 text-center text-lg font-bold text-bg2'>
+      <div className='w-[225px] max-w-full whitespace-nowrap rounded-[200px] border-4 border-solid border-bg2 bg-zinc-50 px-4 py-2 text-center text-lg font-bold text-bg2'>
         {displayOpponent}
       </div>
       {!isChallenger && 'is challenging you '}
